dd8c1130508e57ac69ee62a85ce319e5f335bd16
IOZack <zak@zastudio.net>
2015-02-19 20:25:37 +0000

    Add documentation

--- 11055706.asm
+++ 11055706.asm
@@ -1,5 +1,23 @@
-# Name: Zakaria Abushima
-# ID Number: 11055706
+# calculator.asm -- Assembly language calculator.
+# 
+# Copyright (c) 2015, IOZack.
+#
+# Written by Zakaria Abushima <im@zack.today>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program is a calculator of various operations has been built using Assembly Language.
 
 # Initialise menu options and result strings set
 .data
